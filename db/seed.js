d2be41ba0b9ea4be33ac9cca951ea23d68b5043f
Angie C <a.cajusol98@gmail.com>
2026-03-31 14:58:04 +0200

    seeding

--- db/seed.js
+++ db/seed.js
@@ -7,21 +7,33 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANO
 const seedLocations = async (numEntries) => {
   const locations = [];
 
+  const SODERTALJE_BOUNDS = {
+    minLat: 58.9,
+    maxLat: 59.5,
+    minLng: 17.4,
+    maxLng: 17.8
+  };
   for (let i = 0; i < numEntries; i++) {
     const name = faker.lorem.words(3);
     const type = faker.helpers.arrayElement(['office', 'restaurant', 'train', 'bus']);
     const coordinates = {
-      lat: faker.location.latitude(),
-      lng: faker.location.longitude()
+      lat: faker.location.latitude({
+        min: SODERTALJE_BOUNDS.minLat,
+        max: SODERTALJE_BOUNDS.maxLat
+      }),
+      lng: faker.location.longitude({
+        min: SODERTALJE_BOUNDS.minLng,
+        max: SODERTALJE_BOUNDS.maxLng
+      })
     }
     const description = faker.lorem.sentence();
 
-    
+
     locations.push({
       name: name,
-        type: type,
-        coordinates: coordinates,
-        description: description
+      type: type,
+      coordinates: coordinates,
+      description: description
     });
 
   }
