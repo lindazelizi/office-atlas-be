06c54231607ad27f24be1dc8f60fb70231ab8b14
Linda Zelizi <lindazelizi@gmail.com>
2026-03-31 11:10:09 +0200

    changes

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({ status: "ok" });
+});
+
+app.listen(3001, () => {
+    console.log("Server running on http://localhost:3001");
+});
