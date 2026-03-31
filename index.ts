29f54e32266f7cdb013d9a81209f2ae4d1c84179
Angie C <a.cajusol98@gmail.com>
2026-03-31 13:14:33 +0200

    simple routes

--- index.ts
+++ index.ts
@@ -18,6 +18,31 @@ app.get("/", (req, res) => {
     res.status(200).json({ status: "ok" });
 });
 
+app.get("/locations", async (_, response) => {
+    try {
+        const { data, error } = await supabase.from("locations").select();
+        console.log(data);
+        return response.send(data);
+    } catch (error) {
+        return response.send({ error });
+    }
+});
+
+
+app.get("/locations/:id", async (request, response) => {
+    try {
+        const { data, error } = await supabase
+            .from("locations")
+            .select()
+            .eq("id", request.params.id)
+        console.log(data);
+        return response.send(data);
+    } catch (error) {
+        return response.send({ error });
+    }
+});
+
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
